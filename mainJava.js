966c9fccdc4da53749554bf16522a327c3f1f686
Informejtik300 <wojtyladominik46@gmail.com>
2025-09-26 19:53:41 +0200

    Dodanie funkcji motywów (beta), dodanie baneru

--- mainJava.js
+++ mainJava.js
@@ -7,3 +7,25 @@ function zadzwon() {
     alert("Niestety funkcja dzwonienia jest dostępna tylko na urządzenia mobilne.");
   }
 };
+
+//Zmiana motywu
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+themeToggle.addEventListener('change', function (){
+  body.classList.toggle("bodyDark", themeToggle.checked);
+  if(themeToggle.checked){
+    document.getElementById('phone-ico').href = "Zdjecia/Systemowe/phone-call-white.svg";
+    document.getElementById('mail-box-ico').href = "Zdjecia/Systemowe/email-box-white.svg";
+    document.getElementById('facebook-ico').href = "Zdjecia/Systemowe/facebook-white.svg";
+    document.getElementById('themeIcon').href = "Zdjecia/Systemowe/dark-bright-white.svg";
+    document.getElementById('baner').href = "Zdjecia/Systemowe/banerArtan(szary).png";
+    console.log("Ciemny motyw włączony");
+  } else {
+    document.getElementById('phone-ico').href = "Zdjecia/Systemowe/phone-call-black.svg";
+    document.getElementById('mail-box-ico').href = "Zdjecia/Systemowe/email-box-black.svg";
+    document.getElementById('facebook-ico').href = "Zdjecia/Systemowe/facebook-black.svg";
+    document.getElementById('themeIcon').href = "Zdjecia/Systemowe/dark-bright-black.svg";
+    document.getElementById('baner').href = "Zdjecia/Systemowe/banerArtan(szary).png";
+    console.log("Jasny motyw włączony");
+  }
+});
